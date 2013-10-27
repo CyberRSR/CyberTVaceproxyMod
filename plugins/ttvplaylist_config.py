f3da60af7a789aaca51f139e1fbce8d1053331c2
CyberRSR <molc@ya.ru>
2013-10-27 15:52:18 +0600

    oops

--- plugins/ttvplaylist_config.py
+++ plugins/ttvplaylist_config.py
@@ -1,9 +1,9 @@
 '''
-Raketa-tv.com Playlist Downloader Plugin configuration file
+Torrent-tv.ru Playlist Downloader Plugin configuration file
 '''
 
-# Insert your Raketa-tv.com playlist URL here
-url = 'http://raketa-tv.com/player/JSON/channels_list.json'
+# Insert your Torrent-tv.ru playlist URL here
+url = 'http://api.torrent-tv.ru/t/fQP4RBt%2BalIs7ku%2FZs23raTZQH6%2BG%2BXja65fAMEaDaYGlvhWjijMS4Ak4PIjO%2F5P'
 # Host override. Use this if you're behind NAT.
 # Example: '1.2.3.4'
 host = False
