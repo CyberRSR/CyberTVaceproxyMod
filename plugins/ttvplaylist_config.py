81dc556d1208b2afbe2f4d24dba362d21cdcf704
CyberRSR <molc@ya.ru>
2013-10-27 15:49:45 +0600

    Use free playlist for raketa-tv

--- plugins/ttvplaylist_config.py
+++ plugins/ttvplaylist_config.py
@@ -1,9 +1,9 @@
 '''
-Torrent-tv.ru Playlist Downloader Plugin configuration file
+Raketa-tv.com Playlist Downloader Plugin configuration file
 '''
 
-# Insert your Torrent-tv.ru playlist URL here
-url = ''
+# Insert your Raketa-tv.com playlist URL here
+url = 'http://raketa-tv.com/player/JSON/channels_list.json'
 # Host override. Use this if you're behind NAT.
 # Example: '1.2.3.4'
 host = False
