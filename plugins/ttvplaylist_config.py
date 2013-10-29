c4dc0653824630aca50ba70d3b214548e53e58ed
CyberRSR <molc@ya.ru>
2013-10-29 12:56:19 +0600

    Update ttvplaylist_config.py

--- plugins/ttvplaylist_config.py
+++ plugins/ttvplaylist_config.py
@@ -3,7 +3,7 @@ Torrent-tv.ru Playlist Downloader Plugin configuration file
 '''
 
 # Insert your Torrent-tv.ru playlist URL here
-url = 'http://api.torrent-tv.ru/t/fQP4RBt%2BalIs7ku%2FZs23raTZQH6%2BG%2BXja65fAMEaDaYGlvhWjijMS4Ak4PIjO%2F5P'
+url = ''
 # Host override. Use this if you're behind NAT.
 # Example: '1.2.3.4'
 host = False
