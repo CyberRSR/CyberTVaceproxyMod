a8b8337d7746cfdbd27e78c0094d70292f61b4e2
CyberRSR <molc@ya.ru>
2013-10-31 21:46:40 +0600

    upd for CyberTV playlist

--- plugins/ttvplaylist_config.py
+++ plugins/ttvplaylist_config.py
@@ -2,8 +2,7 @@
 Torrent-tv.ru Playlist Downloader Plugin configuration file
 '''
 
-# Insert your Torrent-tv.ru playlist URL here
-url = ''
-# Host override. Use this if you're behind NAT.
-# Example: '1.2.3.4'
-host = False
+# Insert your Torrent-tv.ru playlist URL here xxx
+url = 'http://api.torrent-tv.ru/t/xxx'
+# Insert your md5pass here xxx.
+host = 'tv.cybertv.zz.mu/get_serv_ch.php?md5pass=xxx&ch_path='
