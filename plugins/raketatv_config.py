efe9cac3a9c6ff75fad60132d34cac7f3b711ff5
CyberRSR <molc@ya.ru>
2013-10-31 21:48:27 +0600

    upd for CyberTV playlist

--- plugins/raketatv_config.py
+++ plugins/raketatv_config.py
@@ -4,6 +4,5 @@ Raketa-tv.com Playlist Downloader Plugin configuration file
 
 # Insert your Raketa-tv.com playlist URL here
 url = 'http://raketa-tv.com/player/JSON/channels_list.json'
-# Host override. Use this if you're behind NAT.
-# Example: '1.2.3.4'
-host = False
+# Insert your md5pass here xxx
+host = 'tv.cybertv.zz.mu/get_serv_ch.php?md5pass=xxx&ch_path='
